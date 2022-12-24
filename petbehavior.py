ea914e9fe58bc7e31be9f621efe0bf18ab9697ba
Plu65 <100289559+Plu65@users.noreply.github.com>
2022-12-24 15:56:55 +0800

    Add files via upload

---
+++ petbehavior.py
@@ -0,0 +1,5 @@
+#===============這些是LINE提供的功能套組，先用import叫出來=============
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import *
+#===============LINEAPI=============================================
